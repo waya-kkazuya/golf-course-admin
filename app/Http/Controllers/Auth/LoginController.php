ec1757a9da6be6abbe231e214ce972542138c817
waya-kkazuya <k.kazuya.for.application@gmail.com>
2026-06-22 16:23:29 +0900

    fix: fix: パスワードのバリデーションルールにstringを追加

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -17,7 +17,7 @@ class LoginController extends Controller
     {
         $credentials = $request->validate([
             'email' => ['required', 'email'],
-            'password' => ['required'],
+            'password' => ['required', 'string'],
         ]);
 
         if (!Auth::attempt($credentials)) {
