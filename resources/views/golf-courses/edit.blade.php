eb122194b3a063012bc1e39c8da9d861a93cfab3
waya-kkazuya <k.kazuya.for.application@gmail.com>
2026-06-26 13:21:29 +0900

    feat: 新規作成画面と編集画面に画像アップロード機能を追加

--- resources/views/golf-courses/edit.blade.php
+++ resources/views/golf-courses/edit.blade.php
@@ -5,12 +5,97 @@
 
     <h1>ゴルフコースを編集</h1>
 
-    <form action="{{ route('golf-courses.update', $golfCourse) }}" method="POST">
+    <form action="{{ route('golf-courses.update', $golfCourse) }}" method="POST" enctype="multipart/form-data">
         @csrf
         @method('PUT')
         @include('golf-courses._form')
+
+
+        {{-- 画像1 --}}
+        <dt><label for="image1">画像1</label></dt>
+        <dd>
+            @if ($golfCourse->image1_url)
+                <div class="image-preview">
+                    <img src="{{ $golfCourse->image1_url }}" alt="現在の画像" style="max-width: 200px;">
+                    <label class="delete-check">
+                        <input type="checkbox" id="delete_image1" name="delete_image1" value="1"
+                            onchange="toggleFileInput('image1', this.checked)">
+                        <span class="delete-label">この画像を削除する</span>
+                    </label>
+                </div>
+            @endif
+            <input type="file" id="image1" name="image1" accept="image/*"
+                onchange="toggleDeleteCheck('image1', this.files.length > 0)">
+            @error('image1')
+                <span class="error">{{ $message }}</span>
+            @enderror
+        </dd>
+
+        {{-- 画像2 --}}
+        <dt><label for="image2">画像2</label></dt>
+        <dd>
+            @if ($golfCourse->image2_url)
+                <div class="image-preview">
+                    <img src="{{ $golfCourse->image2_url }}" alt="現在の画像" style="max-width: 200px;">
+                    <label class="delete-check">
+                        <input type="checkbox" id="delete_image2" name="delete_image2" value="1"
+                            onchange="toggleFileInput('image2', this.checked)">
+                        <span class="delete-label">この画像を削除する</span>
+                    </label>
+                </div>
+            @endif
+            <input type="file" id="image2" name="image2" accept="image/*"
+                onchange="toggleDeleteCheck('image2', this.files.length > 0)">
+            @error('image2')
+                <span class="error">{{ $message }}</span>
+            @enderror
+        </dd>
+
+        {{-- 画像3 --}}
+        <dt><label for="image3">画像3</label></dt>
+        <dd>
+            @if ($golfCourse->image3_url)
+                <div class="image-preview">
+                    <img src="{{ $golfCourse->image3_url }}" alt="現在の画像" style="max-width: 200px;">
+                    <label class="delete-check">
+                        <input type="checkbox" id="delete_image3" name="delete_image3" value="1"
+                            onchange="toggleFileInput('image3', this.checked)">
+                        <span class="delete-label">この画像を削除する</span>
+                    </label>
+                </div>
+            @endif
+            <input type="file" id="image3" name="image3" accept="image/*"
+                onchange="toggleDeleteCheck('image3', this.files.length > 0)">
+            @error('image3')
+                <span class="error">{{ $message }}</span>
+            @enderror
+        </dd>
+
         <button type="submit">更新</button>
     </form>
 
     <p><a href="{{ route('golf-courses.show', $golfCourse) }}">詳細へ</a></p>
+
+    <script>
+        function toggleDeleteCheck(field, hasFile) {
+            const checkbox = document.querySelector('#delete_' + field);
+            if (!checkbox) return;
+            if (hasFile) {
+                checkbox.checked = false;
+                checkbox.disabled = true;
+            } else {
+                checkbox.disabled = false;
+            }
+        }
+
+        function toggleFileInput(field, isChecked) {
+            const fileInput = document.querySelector('#' + field);
+            if (isChecked) {
+                fileInput.value = '';
+                fileInput.disabled = true;
+            } else {
+                fileInput.disabled = false;
+            }
+        }
+    </script>
 </x-layout>
