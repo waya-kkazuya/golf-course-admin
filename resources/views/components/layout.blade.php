93cce6ba235cb227a37de521229c0f84aca1d189
Kazuya <blackcatandbluesky@gmail.com>
2026-06-30 17:21:46 +0900

    feat: 詳細画面にLeafletの地図を表示

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -6,6 +6,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{ $title }}</title>
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+
+    <!-- Leaflet CSS -->
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
+        integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY=" crossorigin="" />
+
     <script src="{{ asset('js/app.js') }}" defer></script>
 </head>
 
@@ -21,6 +26,10 @@
     <div class="container">
         {{ $slot }}
     </div>
+
+    <!-- Leaflet JS（bodyの最後に配置） -->
+    <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"
+        integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo=" crossorigin=""></script>
 </body>
 
 </html>
